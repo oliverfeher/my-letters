3c464660229cdb9406661160fbf3094e846cbc03
oliverfeher <oliver92.feher@gmail.com>
2020-05-26 12:34:48 -0400

    Add test GET root route

--- backend-api/server.js
+++ backend-api/server.js
@@ -2,6 +2,10 @@ const express = require("express");
 
 const app = express();
 
+app.get("/", (req, res) => {
+    res.send("this is working!")
+})
+
 app.listen(3001, () => {
     console.log("Server is running on port 3001")
 });
