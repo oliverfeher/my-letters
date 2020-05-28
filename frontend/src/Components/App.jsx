cec4d062513b9391c768b929cccd298f506f8bae
oliverfeher <oliver92.feher@gmail.com>
2020-05-27 23:35:33 -0400

    Refactor App and create Home and Game component
    - Import react router dom
    - Build front end RR routing

--- frontend/src/Components/App.jsx
+++ frontend/src/Components/App.jsx
@@ -1,24 +1,19 @@
 import React from "react";
 import "../stylesheets/App.css"
-import Logo from "../images/film.png";
-import HeadPhones from "../images/party.png"
+import { BrowserRouter, Route } from "react-router-dom";
+import Home from "./Home";
+import Game from "./Game";
 
 class App extends React.Component
 {
     render()
     {
         return (
-            <div id="main-container">
-                <div id="title-container">
-                    <img src={Logo} id="minion-logo"/>
-                    <div id="title-text-container">
-                        <p style={{fontSize: "7em"}}>Hey <span style={{marginLeft: "4.5%", color: "#FFC107"}}>Kids!</span></p>
-                        <img src={HeadPhones} id="headphones"/>
-                        <p style={{marginLeft: "10%", fontSize: "1.2em"}}>© 2020 - myLetters - All Rights Reserved</p>
-                    </div>
-                </div>
-                <button id="play">PLAY</button>
-            </div>
+            <BrowserRouter>
+                <Route path="/" exact component={Home}/>
+                <Route path="/game" exact component={Game}/>
+
+            </BrowserRouter>
         )
     }
 }
---
+++ frontend/src/Components/Home.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import Logo from "../images/film.png";
+import HeadPhones from "../images/party.png";
+import { Link } from "react-router-dom";
+
+const Home = () =>
+{
+    return (
+        <div id="main-container">
+            <div id="title-container">
+                <img src={Logo} id="minion-logo"/>
+                <div id="title-text-container">
+                    <p style={{fontSize: "7em"}}>Hey <span style={{marginLeft: "4.5%", color: "#FFC107"}}>Kids!</span></p>
+                    <img src={HeadPhones} id="headphones"/>
+                    <p style={{marginLeft: "10%", fontSize: "1.2em"}}>© 2020 - myLetters - All Rights Reserved</p>
+                </div>
+            </div>
+            <Link to="/game" id="play">PLAY</Link>
+        </div>
+    )
+}
+
+export default Home;
