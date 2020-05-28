720ec793edaf1b50e4b01922ebe56ec4e062fd9d
oliverfeher <oliver92.feher@gmail.com>
2020-05-28 11:33:44 -0400

    Build Category component structure
    - Add pictures and titles

--- frontend/src/Components/Categories.jsx
+++ frontend/src/Components/Categories.jsx
@@ -1,10 +1,32 @@
 import React from "react";
+import Math from "../images/math.png"
+import Words from "../images/words.png"
+import Letters from "../images/letters.png"
+
 
 class Categories extends React.Component
 {
     render()
     {
-        return <div></div>
+        return (
+            <div id="categories">
+                <p>Select category!</p>
+                <div id="words-category">
+                    <img></img>
+                    <p></p>
+                </div>
+
+                <div id="math-category">
+                    <img></img>
+                    <p></p>
+                </div>
+
+                <div id="letters-category">
+                    <img></img>
+                    <p></p>
+                </div>
+            </div>
+        )
     }
 }
 
