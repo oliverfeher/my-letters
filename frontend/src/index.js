960e0d45c42b13e322aeed111bb8255c30c0c834
oliverfeher <oliver92.feher@gmail.com>
2020-05-31 16:03:07 -0400

    Refactor redux structure
    - Set up basic variable to implement thunk and middleware
    - Create redux store
    - Init appState

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,9 +3,9 @@ import ReactDOM from 'react-dom';
 import App from './Components/App';
 
 import { Provider } from "react-redux";
-import { applyMiddleware, createStore } from 'redux';
 
-const store = createStore(() => [], {}, applyMiddleware())
+import store from  "./Redux/store";
+
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
