112664331303695d23e978e22dc5b3e6fd2e5412
oliverfeher <oliver92.feher@gmail.com>
2020-05-31 02:19:32 -0400

    Build login component's controlled form

--- frontend/src/Components/Login.jsx
+++ frontend/src/Components/Login.jsx
@@ -1,11 +1,48 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 class Login extends React.Component
 {
     constructor()
     {
         super();
-        this.state = {}
+        this.state = {
+            email: "",
+            password: ""
+        }
+    }
+
+    handleOnChangeEmail = event =>
+    {
+        event.persist();
+        this.setState({
+            email: event.target.value
+        })
+    }
+
+    handleOnChangePassword = event =>
+    {
+        event.persist();
+        this.setState({
+            password: event.target.value
+        })
+    }
+
+    render()
+    {
+        return (
+            <div id="login-signup">
+                <h1>LOGIN</h1>
+                <form id="login-signup" onSubmit={this.handleOnSubmit}>
+                    <label>E-MAIL:</label>
+                    <input type="email" onChange={this.handleOnChangeEmail}/>
+                    <label>PASSWORD:</label>
+                    <input type="password" onChange={this.handleOnChangePassword}/>
+                    <input type="submit" value="LOGIN"/>
+                    <Link to={"/signup"}>SIGNUP</Link>
+                </form>
+            </div>
+        )
     }
 
 }
