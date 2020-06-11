8e8ff1a34f650d351f06ad2a59946780efd6b81c
oliverfeher <oliver92.feher@gmail.com>
2020-06-11 02:19:37 -0400

    Build backend logic for scores/mistakes
    - Implement switch statements for instance methods

--- backend/app/models/User.rb
+++ backend/app/models/User.rb
@@ -5,4 +5,27 @@ class User < ApplicationRecord
     validates :email, uniqueness: true, presence: true
     validates :password_digest, presence: true
 
+    def increase_scores(type)
+        case type
+        when "words"
+            self.words_score += 1
+        when "letters"
+            self.letters_score += 1
+        when "math"
+            self.math_score += 1
+        end
+        self.save
+    end
+
+    def increase_mistakes(type)
+        case type
+        when "words"
+            self.words_mistakes += 1
+        when "letters"
+            self.letters_mistakes += 1
+        when "math"
+            self.math_mistakes += 1
+        end
+        self.save
+    end
 end
